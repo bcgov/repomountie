b2e5032164f81dc632500a788e87f018e2d10905
Jason C. Leach <jason.leach@fullboar.ca>
2018-10-02 13:39:30 -0700

    Fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  // roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.ts?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-}
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.[ts]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src/frontend', 'src/shared'],
+};
