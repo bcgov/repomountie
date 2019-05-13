c381aeeff5e2af309bc1682cb6b9ccbd8a69dcac
Jason C. Leach <jason.leach@fullboar.ca>
2019-05-13 10:06:47 -0700

    Update github appid env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -42,7 +42,7 @@ if (env === 'development') {
 // overrides are always as defined
 nconf.overrides({
   environment: env,
-  githubAppID: process.env.GITHUB_APP_ID,
+  githubAppID: process.env.APP_ID,
   host: process.env.HOST || '127.0.0.1',
   port: process.env.PORT || defaultPort,
   sso: {
