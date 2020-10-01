4f15230b46f42faab30747bc9eb38b5c653dd9d2
Andrew Jun <97andrewjun@gmail.com>
2020-10-01 11:00:09 -0700

    Added prettier-ignore for template literals longer than 120 characters

--- src/libs/handlers.ts
+++ src/libs/handlers.ts
@@ -17,177 +17,194 @@
 // Created by Jason Leach on 2020-03-31.
 //
 
-'use strict'
+'use strict';
 
 import { logger } from '@bcgov/common-nodejs-utils';
 import { Context } from 'probot';
 import { ACCESS_CONTROL } from '../constants';
 import { fetchConfigFile } from './ghutils';
 import { checkForStaleIssues, created } from './issue';
-import { addCollaboratorsToMyIssues, requestUpdateForMyIssues, validatePullRequestIfRequired } from './pullrequest';
-import { addLicenseIfRequired, addMinistryTopicIfRequired, addSecurityComplianceInfoIfRequired, addWordsMatterIfRequire } from './repository';
+import {
+  addCollaboratorsToMyIssues,
+  requestUpdateForMyIssues,
+  validatePullRequestIfRequired,
+} from './pullrequest';
+import {
+  addLicenseIfRequired,
+  addMinistryTopicIfRequired,
+  addSecurityComplianceInfoIfRequired,
+  addWordsMatterIfRequire,
+} from './repository';
 
 export const memberAddedOrEdited = async (context: Context): Promise<void> => {
-    const owner = context.payload.organization.login;
-    const repo = context.payload.repository.name;
+  const owner = context.payload.organization.login;
+  const repo = context.payload.repository.name;
 
-    try {
-        await addCollaboratorsToMyIssues(context, owner, repo);
-    } catch (err) {
-        const message = 'Unable to handel member event';
-        logger.error(`${message}, error = ${err.message}`);
-    }
-}
+  try {
+    await addCollaboratorsToMyIssues(context, owner, repo);
+  } catch (err) {
+    const message = 'Unable to handel member event';
+    logger.error(`${message}, error = ${err.message}`);
+  }
+};
 
 export const pullRequestOpened = async (context: Context): Promise<void> => {
-    const owner = context.payload.organization.login;
-    const repo = context.payload.repository.name;
-    const isFromBot = context.isBot;
+  const owner = context.payload.organization.login;
+  const repo = context.payload.repository.name;
+  const isFromBot = context.isBot;
 
-    if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
-        logger.info(
+  if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
+    // prettier-ignore
+    logger.info(
             `Skipping PR ${context.payload.pull_request.number} for repo ${context.payload.repository.name
             } because its not from an allowed installation`
         );
 
-        return;
-    }
+    return;
+  }
 
-    if (isFromBot) {
-        try {
-            await addCollaboratorsToMyIssues(context, owner, repo);
-        } catch (err) {
-            const message = `Unable to assign collaborators in ${repo}`;
-            logger.error(`${message}, error = ${err.message}`);
-        }
-
-        return;
-    }
-
-    logger.info(
-        `Processing PR ${context.payload.pull_request.number} for repo ${context.payload.repository.name
-        }`
-    );
-
+  if (isFromBot) {
     try {
-        const rmconfig = await fetchConfigFile(context);
-
-        await validatePullRequestIfRequired(context, rmconfig);
+      await addCollaboratorsToMyIssues(context, owner, repo);
     } catch (err) {
-        logger.info(`Unable to handle pull request, err = ${err.message}`);
+      const message = `Unable to assign collaborators in ${repo}`;
+      logger.error(`${message}, error = ${err.message}`);
     }
-}
+
+    return;
+  }
+
+  logger.info(
+    `Processing PR ${context.payload.pull_request.number} for repo ${context.payload.repository.name}`
+  );
+
+  try {
+    const rmconfig = await fetchConfigFile(context);
+
+    await validatePullRequestIfRequired(context, rmconfig);
+  } catch (err) {
+    logger.info(`Unable to handle pull request, err = ${err.message}`);
+  }
+};
 
 export const issueCommentCreated = async (context: Context): Promise<void> => {
-    const owner = context.payload.organization.login;
-    const isFromBot = context.isBot;
+  const owner = context.payload.organization.login;
+  const isFromBot = context.isBot;
 
-    if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
-        logger.info(
+  if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
+    // prettier-ignore
+    logger.info(
             `Skipping issue ${context.payload.issue.number} for repo ${context.payload.repository.name
             } because its not from an allowed installation`
         );
 
-        return;
-    }
+    return;
+  }
 
-    // This can throw a `TypeError` during testing.
-    if (isFromBot) {
-        // Don't act crazy.
-        logger.info(`Skipping issue ${context.payload.issue.id} because its from a bot`);
-        return;
-    }
+  // This can throw a `TypeError` during testing.
+  if (isFromBot) {
+    // Don't act crazy.
+    logger.info(
+      `Skipping issue ${context.payload.issue.id} because its from a bot`
+    );
+    return;
+  }
 
-    logger.info(`Processing issue ${context.payload.issue.id}`);
+  logger.info(`Processing issue ${context.payload.issue.id}`);
 
+  try {
+    await created(context);
+  } catch (err) {
+    logger.error(`Unable to process issue ${context.payload.issue.id}`);
+  }
+};
+
+export const repositoryScheduled = async (
+  context: Context,
+  scheduler: any
+): Promise<void> => {
+  logger.info(`Processing ${context.payload.repository.name}`);
+
+  const owner = context.payload.installation.account.login;
+  const repo = context.payload.repository.name;
+
+  if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
+    logger.info(
+      `Skipping scheduled repository ${repo} because its not part of an allowed installation`
+    );
+    return;
+  }
+
+  if (context.payload.repository.archived) {
+    logger.warn(`The repo ${repo} is archived. Skipping.`);
+    scheduler.stop(context.payload.repository);
+
+    return;
+  }
+
+  try {
+    // These are all independent func. Rather than call them each in a
+    // try/catch I'm bundling them. If a repo is created without a
+    // README etc then it will not have a master branch and the PRs for
+    // licensing and compliance cannot be created; this is why these
+    // two functions are below and not in `repositoryCreated`.
+    await Promise.all([
+      addLicenseIfRequired(context, owner, repo),
+      addSecurityComplianceInfoIfRequired(context, owner, repo),
+      addCollaboratorsToMyIssues(context, owner, repo),
+      requestUpdateForMyIssues(context, owner, repo),
+    ]);
+  } catch (err) {
+    const message = `Unable to complete all housekeeping tasks, repo is ${repo}`;
+    logger.error(`${message}, error = ${err.message}`);
+  }
+
+  try {
+    // functionality below here requires a `config` file
+    // exist in the repo.
     try {
-        await created(context);
+      const rmconfig = await fetchConfigFile(context);
+      await checkForStaleIssues(context, rmconfig);
     } catch (err) {
-        logger.error(`Unable to process issue ${context.payload.issue.id}`);
+      logger.info('No config file. Skipping.');
     }
-}
-
-export const repositoryScheduled = async (context: Context, scheduler: any): Promise<void> => {
-    logger.info(`Processing ${context.payload.repository.name}`);
-
-    const owner = context.payload.installation.account.login;
-    const repo = context.payload.repository.name;
-
-    if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
-        logger.info(
-            `Skipping scheduled repository ${repo
-            } because its not part of an allowed installation`
-        );
-        return;
-    }
-
-    if (context.payload.repository.archived) {
-        logger.warn(`The repo ${repo} is archived. Skipping.`);
-        scheduler.stop(context.payload.repository);
-
-        return;
-    }
-
-    try {
-        // These are all independent func. Rather than call them each in a
-        // try/catch I'm bundling them. If a repo is created without a
-        // README etc then it will not have a master branch and the PRs for
-        // licensing and compliance cannot be created; this is why these
-        // two functions are below and not in `repositoryCreated`.
-        await Promise.all([
-            addLicenseIfRequired(context, owner, repo),
-            addSecurityComplianceInfoIfRequired(context, owner, repo),
-            addCollaboratorsToMyIssues(context, owner, repo),
-            requestUpdateForMyIssues(context, owner, repo),
-        ]);
-    } catch (err) {
-        const message = `Unable to complete all housekeeping tasks, repo is ${repo}`;
-        logger.error(`${message}, error = ${err.message}`);
-    }
-
-    try {
-        // functionality below here requires a `config` file
-        // exist in the repo.
-        try {
-            const rmconfig = await fetchConfigFile(context);
-            await checkForStaleIssues(context, rmconfig);
-        } catch (err) {
-            logger.info('No config file. Skipping.');
-        }
-    } catch (err) {
-        const message = `Unable to process repository ${repo}`;
-        logger.error(`${message}, error = ${err.message}`);
-    }
-}
+  } catch (err) {
+    const message = `Unable to process repository ${repo}`;
+    logger.error(`${message}, error = ${err.message}`);
+  }
+};
 
 export const repositoryCreated = async (context: Context): Promise<void> => {
-    logger.info(`Processing ${context.payload.repository.name}`);
+  logger.info(`Processing ${context.payload.repository.name}`);
 
-    const owner = context.payload.repository.owner.login;
-    const repo = context.payload.repository.name;
+  const owner = context.payload.repository.owner.login;
+  const repo = context.payload.repository.name;
 
-    if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
-        logger.info(
-            `Skipping repository ${repo} because its not part of an allowed installation`
-        );
-        return;
-    }
+  if (!ACCESS_CONTROL.allowedInstallations.includes(owner)) {
+    logger.info(
+      `Skipping repository ${repo} because its not part of an allowed installation`
+    );
+    return;
+  }
 
-    try {
-        // These are all independent func. Rather than call them
-        // each in a try/catch I'm bundling them.
-        await Promise.all([
-            // addLicenseIfRequired(context, owner, repo),
-            // addSecurityComplianceInfoIfRequired(context, owner, repo),
-            addMinistryTopicIfRequired(context, owner, repo),
-            addWordsMatterIfRequire(context, owner, repo),
-        ]);
-    } catch (err) {
-        const message = `Unable to complete all housekeeping tasks, repo is ${repo}`;
-        logger.error(`${message}, error = ${err.message}`);
-    }
-}
+  try {
+    // These are all independent func. Rather than call them
+    // each in a try/catch I'm bundling them.
+    await Promise.all([
+      // addLicenseIfRequired(context, owner, repo),
+      // addSecurityComplianceInfoIfRequired(context, owner, repo),
+      addMinistryTopicIfRequired(context, owner, repo),
+      addWordsMatterIfRequire(context, owner, repo),
+    ]);
+  } catch (err) {
+    const message = `Unable to complete all housekeeping tasks, repo is ${repo}`;
+    logger.error(`${message}, error = ${err.message}`);
+  }
+};
 
-export const repositoryDeleted = async (context: Context, scheduler: any): Promise<void> => {
-    scheduler.stop(context.payload.repository);
-}
+export const repositoryDeleted = async (
+  context: Context,
+  scheduler: any
+): Promise<void> => {
+  scheduler.stop(context.payload.repository);
+};
